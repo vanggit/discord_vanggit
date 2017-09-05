81731047f07349e04ef519e848c8591ed7199038
vanggit <>
2017-09-05 19:45:19 +0300

    anonimizer 0.0.1 - the result of a pair of hours of monkeying around with no real aim

--- index.js
+++ index.js
@@ -8,9 +8,22 @@ client.on('ready', () => {
 });
  
 client.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong');
+  if (message.content[0] !== '%' && !(message.author.bot)) {
+    message.delete();
+    // message.author = null;
+    // message.client = null;
+    message.channel.send(message.content);
+    //message.reply(message.content, {color: "#ff0000", prepend: "prepend", reply: "reply"});
+  }else if(message.content[0] === '%'){
+    let toReplaceArr = message.content.split("");
+    toReplaceArr.shift();
+    let toReplaceStr = toReplaceArr.join("");
+    message.edit(toReplaceStr);
+  }else if(message.content[0] === '!'){
+    message.delete();
   }
 });
  
 client.login(Secrets.token);
+
+module.exports = client;
